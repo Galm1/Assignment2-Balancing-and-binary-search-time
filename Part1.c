8f7e48ec7e00f1731e5b2bf8eae5356b6bc5691a
Elisha Garcia <garciaelisha93@yahoo.com>
2023-09-14 10:42:22 -0500

    Add files via upload
    organizing thoughts on problem

---
+++ Part1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+//assignment is to take a sting of 6 paranthesis and output whether or not the paranthesisare "balenced" or not.
+//such as "{}" is balenced and "{{" in not
+
+int main () 
+{
+
+	//need to take string of parenthesis to be array we can scan
+
+	//need to have a varible that hold a value, the value changes based on what is read
+	//for that we will need to make "{" to be a +1 and "}" to be a -1
+	//if the value is not 0 (or whatever our starting point is) then it is un balenced and we state so with printf along with the output
+	//if the value is 0 (or whatever we state as starting point) then it is balenced and we printf so along with the output
+	// at some point we need to ask the user to enter 6 paranthesis at random and then run the program and output correct result
+	
+
+}
