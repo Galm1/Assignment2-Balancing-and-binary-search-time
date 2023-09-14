4990e8bf42388a1500772579f35a0d6ba6371525
Elisha Garcia <garciaelisha93@yahoo.com>
2023-09-14 12:12:05 -0500

    maybe i just use STACK?

--- Part1.c
+++ Part1.c
@@ -7,6 +7,10 @@
 int main () 
 {
 
+	char arr[] = {"{", "}"};
+	int balencePoint = 0;
+	
+	printf("test output: %s" , string );
 	//need to take string of parenthesis to be array we can scan
 
 	//need to have a varible that hold a value, the value changes based on what is read
@@ -14,6 +18,9 @@ int main ()
 	//if the value is not 0 (or whatever our starting point is) then it is un balenced and we state so with printf along with the output
 	//if the value is 0 (or whatever we state as starting point) then it is balenced and we printf so along with the output
 	// at some point we need to ask the user to enter 6 paranthesis at random and then run the program and output correct result
+	
+	
+	//ok but why do any of the above when we can just use STACK? idk maybe we do both... but prob STACK
 	
 	
 
