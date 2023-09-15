4a7d55515f336992130d3b7d2a1c296e94e8bca4
Elisha Garcia <garciaelisha93@yahoo.com>
2023-09-15 12:40:23 -0500

    trying for user input into arr then printf

--- Part1.c
+++ Part1.c
@@ -6,15 +6,31 @@
 
 int main () 
 {
-
-	char arr1[2] = {'{','}'};
-	int balencePoint = 0;
 	
-	printf("test output: ");
+	int MAX = 6;
+	char stack[MAX];
+	int top = -1;
+	char arr2[MAX];
+	//char arr1[2] = {'{','}'};
+	//int balencePoint = 0;
 	
-	for (int i = 0; i < 2; i++){
-	printf("%c" ,  arr1[i]);
+	printf("Enter 6 paranthesis then press ENTER: ");
+	
+	for (int i = 0; i < 6; i++)
+	{
+		scanf("%c, ", &(arr2[i]));
 	}
+	
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%c, ", arr2[i]);
+	}
+	
+	//printf("test output: ");
+	
+	/*for (int i = 0; i < 2; i++){
+	printf("%c" ,  arr1[i]);
+	} */
 	//need to take string of parenthesis to be array we can scan
 
 	//need to have a varible that hold a value, the value changes based on what is read
