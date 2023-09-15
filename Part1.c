ee33b93c27a8b96b676c95102216bbad77c3d1c4
Elisha Garcia <garciaelisha93@yahoo.com>
2023-09-15 18:29:48 -0500

    added stack print. tried to implement stack. not working as intended.

--- Part1.c
+++ Part1.c
@@ -24,7 +24,7 @@
 	int peek() 
 	{
 		if (isEmpty())
-			printf("Cold not retrieve, Stack is empty\n");
+			printf("\npeek() could not retrieve, Stack is empty\n");
 		else
 			return stack[top];
 	}
@@ -38,7 +38,7 @@
 			top = top - 1;
 			return data;
 		} else {
-			printf("Could not retrieve, Stack is empty\n");
+			printf("pop() could not retrieve, Stack is empty\n");
 		}
 	}	
 	
@@ -49,11 +49,11 @@
 			top = top + 1;
 			stack[top] = data;
 		} else {
-			printf("Could not retrieve, Stack is full\n");
+			printf("\n push() Could not retrieve, Stack is full\n");
 		}
 	}
-//assignment is to take a sting of 6 paranthesis and output whether or not the paranthesisare "balenced" or not.
-//such as "{}" is balenced and "{{" in not
+
+
 
 int main () 
 {
@@ -65,6 +65,12 @@ int main ()
 	for (int i = 0; i < MAX; i++)
 	{
 		scanf("%c, ", &(arr1[i]));
+		
+		if (arr1[i] == '{')
+		{
+			push(arr1[i]);
+		} else
+			pop();
 	}
 	
 	
@@ -73,28 +79,12 @@ int main ()
 		printf("%c, ", arr1[i]);
 	}
 	
+	printf("\ntop of stack: %c\nStack: ", peek());
 	
-	
-	
-	
-	
-	
-	
-	//printf("test output: ");
-	
-	/*for (int i = 0; i < 2; i++){
-	printf("%c" ,  arr1[i]);
-	} */
-	//need to take string of parenthesis to be array we can scan
-
-	//need to have a varible that hold a value, the value changes based on what is read
-	//for that we will need to make "{" to be a +1 and "}" to be a -1
-	//if the value is not 0 (or whatever our starting point is) then it is un balenced and we state so with printf along with the output
-	//if the value is 0 (or whatever we state as starting point) then it is balenced and we printf so along with the output
-	// at some point we need to ask the user to enter 6 paranthesis at random and then run the program and output correct result
-	
-	
-	//ok but why do any of the above when we can just use STACK? idk maybe we do both... but prob STACK
+	for (int i = 0; i < MAX; i++)
+	{
+		printf("%d.%c, ", i+1, stack[i]);
+	}
 	
 	
 
