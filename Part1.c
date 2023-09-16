c474254bee2cb0ac36914137238478bd396c49e0
Elisha Garcia <garciaelisha93@yahoo.com>
2023-09-16 07:52:31 -0500

    modify and works
    check for empty and full stack. check wrong input characters. check if stack is empty to check if it is balanced or not.

--- Part1.c
+++ Part1.c
@@ -1,91 +1,86 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdbool.h>
 
-	int MAX = 6;
-	char stack[6];
-	int top = -1;
+#define MAX 6
 
+char stack[MAX];
+int top = -1;
 
-
-	int isEmpty()
-	{
-		if (top == -1)
-			return 1;
-		return 0;
-	}
-	
-	int isFull()
-	{
-		if(top == MAX)
-			return 1;
-		return 0;			
-	}
-	
-	int peek() 
-	{
-		if (isEmpty())
-			printf("\npeek() could not retrieve, Stack is empty\n");
-		else
-			return stack[top];
-	}
-	
-	int pop()
-	{
-		int data;
-		if (!isEmpty())
-		{
-			data = stack[top];
-			top = top - 1;
-			return data;
-		} else {
-			printf("pop() could not retrieve, Stack is empty\n");
-		}
-	}	
-	
-	int push(int data)
-	{
-		if(!isFull())
-		{
-			top = top + 1;
-			stack[top] = data;
-		} else {
-			printf("\n push() Could not retrieve, Stack is full\n");
-		}
-	}
-
-
-
-int main () 
+bool isEmpty() 
 {
-	
-	char arr1[MAX];
-	
-	printf("Enter 6 paranthesis then press ENTER: ");
-	
-	for (int i = 0; i < MAX; i++)
-	{
-		scanf("%c, ", &(arr1[i]));
-		
-		if (arr1[i] == '{')
-		{
-			push(arr1[i]);
-		} else
-			pop();
-	}
-	
-	
-	for (int i = 0; i < MAX; i++)
-	{
-		printf("%c, ", arr1[i]);
-	}
-	
-	printf("\ntop of stack: %c\nStack: ", peek());
-	
-	for (int i = 0; i < MAX; i++)
-	{
-		printf("%d.%c, ", i+1, stack[i]);
-	}
-	
-	
+    return top == -1;
+}
 
+bool isFull() 
+{
+    return top == MAX - 1;
+}
+
+char peek() 
+{
+    if (!isEmpty())
+	{
+        return stack[top];
+    } else {
+        printf("peek() could not retrieve, Stack is empty\n");
+        return '\0'; // Return null for an empty stack
+    }
+}
+
+char pop() 
+{
+    if (!isEmpty()) 
+	{
+        return stack[top--];
+    } else {
+        printf("pop() could not retrieve, Stack is empty\n");
+        return '\0'; // Return null for an an empty stack
+    }
+}
+
+void push(char data) 
+{
+    if (!isFull()) 
+	{
+        stack[++top] = data;
+    } else {
+        printf("\npush() could not retrieve, Stack is full\n");
+    }
+}
+
+
+
+
+
+int main() {
+    char arr1[MAX];
+
+    printf("Enter 6 parentheses then press ENTER: ");
+
+    for (int i = 0; i < MAX; i++) {
+        scanf(" %c", &(arr1[i])); 
+    }
+
+    for (int i = 0; i < MAX; i++) {
+        if (arr1[i] == '{') {
+            push(arr1[i]);
+        } else if (arr1[i] == '}') {
+            if (isEmpty()) {
+                printf("Invalid Parenthesis Expression\n");
+                return 1;
+            }
+            pop();
+        } else {
+            printf("Invalid input\n");
+            return 1;
+        }
+    }
+
+    if (isEmpty()) {
+        printf("Valid Parenthesis Expression\n");
+    } else {
+        printf("Invalid Parenthesis Expression\n");
+    }
+
+    return 0;
 }
